77be4691de2b488c3d65c670fe12433a4ad20348
k8hertweck <k8hertweck@gmail.com>
2017-07-04 12:03:52 -0500

    updating paths and color assignment

--- mt_genome.R
+++ mt_genome.R
@@ -12,9 +12,9 @@ mt <- grch38 %>%
 mt$symbol <- gsub("MT-", "", mt$symbol)
 # add colors for protein vs tRNA
 mt <- mutate(mt, color=biotype) # add duplicate column with new name
-mt$color <- gsub("protein_coding", "blue", mt$color)
-mt$color <- gsub("Mt_tRNA", "green", mt$color)
-mt$color <- gsub("Mt_rRNA", "purple", mt$color)
+mt$color <- gsub("protein_coding", "fill_color=blue", mt$color)
+mt$color <- gsub("Mt_tRNA", "fill_color=green", mt$color)
+mt$color <- gsub("Mt_rRNA", "fill_color=purple", mt$color)
 
 # highlight files
 # sense
@@ -23,20 +23,20 @@ mtSense <- mt %>%
   dplyr::select(chr, start, end, color, symbol)
 # save to highlight table
 mtSenseTrim <- dplyr::select(mtSense, chr, start, end, color)
-write.table(mtSenseTrim, "circos/highlight_sense.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
+write.table(mtSenseTrim, "circos/data/highlight_sense.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
 
 # antisense
 mtAnti <- mt %>%
   filter(strand == -1) %>%
   dplyr::select(chr, start, end, color, symbol)
 # add in D-loop
-mtAnti <- rbind(mtAnti, c("MT", "1", "576", "yellow", "D-loop"))
-mtAnti <- rbind(mtAnti, c("MT", "16024", "16569", "yellow", "D-loop"))
+mtAnti <- rbind(mtAnti, c("MT", "1", "576", "fill_color=yellow", "D-loop"))
+mtAnti <- rbind(mtAnti, c("MT", "16024", "16569", "fill_color=yellow", "D-loop"))
 # save to highlight table
 mtAntiTrim <- dplyr::select(mtAnti, chr, start, end, color)
-write.table(mtAntiTrim, "circos/highlight_antisense.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
+write.table(mtAntiTrim, "circos/data/highlight_antisense.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
 
 # gene label file
 mtGenes <- rbind(mtSense, mtAnti)
 mtGenes <- dplyr::select(mtGenes, chr, start, end, symbol)
-write.table(mtGenes, "circos/gene_labels.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
+write.table(mtGenes, "circos/data/gene_labels.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
