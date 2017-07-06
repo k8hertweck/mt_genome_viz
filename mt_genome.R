e1e5a8043be529eee1d7a85529c3d4ee7d0d3380
k8hertweck <k8hertweck@gmail.com>
2017-07-05 23:07:55 -0500

    tweaking colors

--- mt_genome.R
+++ mt_genome.R
@@ -12,9 +12,22 @@ mt <- grch38 %>%
 mt$symbol <- gsub("MT-", "", mt$symbol)
 # add colors for protein vs tRNA
 mt <- mutate(mt, color=biotype) # add duplicate column with new name
-mt$color <- gsub("protein_coding", "fill_color=blue", mt$color)
-mt$color <- gsub("Mt_tRNA", "fill_color=green", mt$color)
-mt$color <- gsub("Mt_rRNA", "fill_color=purple", mt$color)
+# color by type of gene (or complex)
+mt$color <- gsub("Mt_tRNA", "fill_color=vlgrey", mt$color)
+mt$color <- gsub("Mt_rRNA", "fill_color=lorange", mt$color)
+#mt$color <- gsub("protein_coding", "fill_color=vlgreen", mt$color)
+# color protein coding genes by complex
+com1 <- filter(mt, grepl("ND", symbol))
+com1$color <- gsub("protein_coding", "fill_color=purple", com1$color)
+com3 <- filter(mt, grepl("CYB", symbol))
+com3$color <- gsub("protein_coding", "fill_color=yellow", com3$color)
+com4 <- filter(mt, grepl("CO", symbol))
+com4$color <- gsub("protein_coding", "fill_color=dblue", com4$color)
+com5 <- filter(mt, grepl("ATP", symbol))
+com5$color <- gsub("protein_coding", "fill_color=dgreen", com5$color)
+# recombine color coded data
+other <- filter(mt, color != "protein_coding")
+mt <- rbind(other, com1, com3, com4, com5)
 
 # highlight files
 # sense
@@ -29,8 +42,8 @@ mtAnti <- mt %>%
   filter(strand == -1) %>%
   dplyr::select(chr, start, end, symbol, color)
 # add in D-loop
-mtAnti <- rbind(mtAnti, c("MT", "1", "576", "D-loop", "fill_color=yellow"))
-mtAnti <- rbind(mtAnti, c("MT", "16024", "16569", "D-loop", "fill_color=yellow"))
+mtAnti <- rbind(mtAnti, c("MT", "1", "576", "D-loop", "fill_color=dred"))
+mtAnti <- rbind(mtAnti, c("MT", "16024", "16569", "D-loop", "fill_color=dred"))
 # save to highlight table
 write.table(mtAnti, "circos/data/highlight_antisense.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
 
