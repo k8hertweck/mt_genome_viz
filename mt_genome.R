26be7182b779888d6f181ca3b21ced27aa8b6a62
k8hertweck <k8hertweck@gmail.com>
2017-07-05 22:12:05 -0500

    correcting order of labels

--- mt_genome.R
+++ mt_genome.R
@@ -37,10 +37,12 @@ write.table(mtAnti, "circos/data/highlight_antisense.txt", row.names=FALSE, col.
 # gene label file
 # combine sense and antisense
 mtGenes <- rbind(mtSense, mtAnti)
+mtGenes$start <- as.numeric(mtGenes$start)
+mtGenes <- mtGenes[order(mtGenes$start),]
 # find full gene names to match symbols
 names <- read.table("rCRS.gb.txt", sep="\t", skip=85)
 names <- unique(filter(names, grepl("gene=", V1)))
-names <- c(gsub("^.*gene=", "", names$V1, perl=TRUE), "D-loop", "D-loop")
+names <- c("D-loop", gsub("^.*gene=", "", names$V1, perl=TRUE), "D-loop")
 mtGenes <- cbind(mtGenes, names)
 mtGenes <- dplyr::select(mtGenes, chr, start, end, names)
 write.table(mtGenes, "circos/data/gene_labels.txt", row.names=FALSE, col.names = FALSE, quote = FALSE)
